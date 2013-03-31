e9b211011296eebb4e6de085b00798bc3bcac29f
Jesse Rosalia <jesse.rosalia@gatech.edu>
2013-03-31 18:29:50 -0400

    Added code to let us append the dataset to an arff file.  This involves letting the DataSetWriter append (instead of overwrite) and also letting the DataSetWriter accept an array of classnames.

--- src/shared/DataSetWriter.java
+++ src/shared/DataSetWriter.java
@@ -21,30 +21,77 @@ public class DataSetWriter {
     private String filename;
 
     /**
+     * True to append the results to the end of
+     *  the file, false to overwrite.  This is
+     *  useful if we need to write some kind of
+     *  header to the file before writing the
+     *  dataset.
+     * 
+     */
+    private boolean append;
+
+    private String[] labelStrings;
+
+    /**
      * Make a new data set writer
      * @param set the data set to writer
      */
     public DataSetWriter(DataSet set, String filename) {
         this.set = set;
         this.filename = filename;
+        this.append = false;
+        this.labelStrings = null;
     }
     
+    /**
+     * Make a new data set writer
+     * @param set the data set to writer
+     */
+    public DataSetWriter(DataSet set, String filename, boolean append) {
+        this.set = set;
+        this.filename = filename;
+        this.append = append;
+        this.labelStrings = null;
+    }
+
+    /**
+     * Make a new data set writer
+     * @param set the data set to writer
+     */
+    public DataSetWriter(DataSet set, String filename, boolean append, String[] labelStrings) {
+        this.set = set;
+        this.filename = filename;
+        this.append = append;
+        this.labelStrings = labelStrings;
+    }
+
     /**
      * Write the file out
      * @throws IOException when something goes bad
      */
     public void write() throws IOException {
-        PrintWriter pw = new PrintWriter(new FileWriter(filename));
+        PrintWriter pw = new PrintWriter(new FileWriter(filename, this.append));
         for (int i = 0; i < set.size(); i++) {
             Instance data = set.get(i);
+            boolean label = false;
             while (data != null) {
-                for (int j = 0; j < data.size(); j++) {
-                    pw.print(data.getContinuous(j));
-                    if (j + 1 < data.size() || data.getLabel() != null) {
-                        pw.print(", ");
+                if (label && this.labelStrings != null) {
+                    for (int j = 0; j < data.size(); j++) {
+                        pw.print(this.labelStrings[data.getDiscrete(j)]);
+                        if (j + 1 < data.size() || data.getLabel() != null) {
+                            pw.print(", ");
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < data.size(); j++) {
+                        pw.print(data.getContinuous(j));
+                        if (j + 1 < data.size() || data.getLabel() != null) {
+                            pw.print(", ");
+                        }
                     }
                 }
                 data = data.getLabel();
+                label = true;
             }
             pw.println();
         }
