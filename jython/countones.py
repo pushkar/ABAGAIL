44e6089a58b9c36c4e5965a863fde8bd7f2f3ec5
michael <mike@mike.com>
2014-10-01 09:40:40 +0300

    add jython examples

---
+++ jython/countones.py
@@ -0,0 +1,78 @@
+import sys
+import os
+import time
+
+import java.io.FileReader as FileReader
+import java.io.File as File
+import java.lang.String as String
+import java.lang.StringBuffer as StringBuffer
+import java.lang.Boolean as Boolean
+import java.util.Random as Random
+
+import dist.DiscreteDependencyTree as DiscreteDependencyTree
+import dist.DiscreteUniformDistribution as DiscreteUniformDistribution
+import dist.Distribution as Distribution
+import opt.DiscreteChangeOneNeighbor as DiscreteChangeOneNeighbor
+import opt.EvaluationFunction as EvaluationFunction
+import opt.GenericHillClimbingProblem as GenericHillClimbingProblem
+import opt.HillClimbingProblem as HillClimbingProblem
+import opt.NeighborFunction as NeighborFunction
+import opt.RandomizedHillClimbing as RandomizedHillClimbing
+import opt.SimulatedAnnealing as SimulatedAnnealing
+import opt.example.FourPeaksEvaluationFunction as FourPeaksEvaluationFunction
+import opt.ga.CrossoverFunction as CrossoverFunction
+import opt.ga.SingleCrossOver as SingleCrossOver
+import opt.ga.DiscreteChangeOneMutation as DiscreteChangeOneMutation
+import opt.ga.GenericGeneticAlgorithmProblem as GenericGeneticAlgorithmProblem
+import opt.ga.GeneticAlgorithmProblem as GeneticAlgorithmProblem
+import opt.ga.MutationFunction as MutationFunction
+import opt.ga.StandardGeneticAlgorithm as StandardGeneticAlgorithm
+import opt.ga.UniformCrossOver as UniformCrossOver
+import opt.prob.GenericProbabilisticOptimizationProblem as GenericProbabilisticOptimizationProblem
+import opt.prob.MIMIC as MIMIC
+import opt.prob.ProbabilisticOptimizationProblem as ProbabilisticOptimizationProblem
+import shared.FixedIterationTrainer as FixedIterationTrainer
+import opt.example.CountOnesEvaluationFunction as CountOnesEvaluationFunction
+from array import array
+
+
+
+
+"""
+Commandline parameter(s):
+   none
+"""
+
+N=80
+fill = [2] * N
+ranges = array('i', fill)
+
+ef = CountOnesEvaluationFunction()
+odd = DiscreteUniformDistribution(ranges)
+nf = DiscreteChangeOneNeighbor(ranges)
+mf = DiscreteChangeOneMutation(ranges)
+cf = SingleCrossOver()
+df = DiscreteDependencyTree(.1, ranges)
+hcp = GenericHillClimbingProblem(ef, odd, nf)
+gap = GenericGeneticAlgorithmProblem(ef, odd, mf, cf)
+pop = GenericProbabilisticOptimizationProblem(ef, odd, df)
+
+rhc = RandomizedHillClimbing(hcp)
+fit = FixedIterationTrainer(rhc, 200)
+fit.train()
+print "RHC: " + str(ef.value(rhc.getOptimal()))
+
+sa = SimulatedAnnealing(100, .95, hcp)
+fit = FixedIterationTrainer(sa, 200)
+fit.train()
+print "SA: " + str(ef.value(sa.getOptimal()))
+
+ga = StandardGeneticAlgorithm(20, 20, 0, gap)
+fit = FixedIterationTrainer(ga, 300)
+fit.train()
+print "GA: " + str(ef.value(ga.getOptimal()))
+
+mimic = MIMIC(50, 10, pop)
+fit = FixedIterationTrainer(mimic, 100)
+fit.train()
+print "MIMIC: " + str(ef.value(mimic.getOptimal()))
---
+++ jython/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# edit the classpath to to the location of your ABAGAIL jar file
+#
+export CLASSPATH=./lib/ABAGAIL.jar:$CLASSPATH
+mkdir -p data/plot logs image
+
+# four peaks
+echo "four peaks"
+jython fourpeaks.py
+
+# count ones
+echo "count ones"
+jython countones.py
+
+# continuous peaks
+echo "continuous peaks"
+jython continuouspeaks.py
+
+# knapsack
+echo "Running knapsack"
+jython knapsack.py
+
