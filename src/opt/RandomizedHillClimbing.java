9261733c5e146e57da1b457555950bcac6377c96
jlas <juan.lasheras@gmail.com>
2015-03-15 08:53:09 -0400

    Add random-restart logic to RHC & SA

--- src/opt/RandomizedHillClimbing.java
+++ src/opt/RandomizedHillClimbing.java
@@ -8,17 +8,27 @@ import shared.Instance;
  * @version 1.0
  */
 public class RandomizedHillClimbing extends OptimizationAlgorithm {
-    
+
     /**
      * The current optimization data
      */
     private Instance cur;
-    
+
     /**
      * The current value of the data
      */
     private double curVal;
-    
+
+    // Track the best instance / value seen
+    private Instance best;
+    private double bestVal;
+
+    // Max number of iterations to go without finding a better max
+    private int SINCE_NEW_MAX = 10;
+
+    // Keep track of iterations since we found a new max
+    private int sinceNewCount = 0;
+
     /**
      * Make a new randomized hill climbing
      */
@@ -26,6 +36,8 @@ public class RandomizedHillClimbing extends OptimizationAlgorithm {
         super(hcp);
         cur = hcp.random();
         curVal = hcp.value(cur);
+        best = cur;
+        bestVal = curVal;
     }
 
     /**
@@ -33,13 +45,29 @@ public class RandomizedHillClimbing extends OptimizationAlgorithm {
      */
     public double train() {
         HillClimbingProblem hcp = (HillClimbingProblem) getOptimizationProblem();
+
+        if (sinceNewCount > SINCE_NEW_MAX) {
+            cur = hcp.random();
+            curVal = hcp.value(cur);
+            sinceNewCount = 0;
+        }
+
         Instance neigh = hcp.neighbor(cur);
         double neighVal = hcp.value(neigh);
         if (neighVal > curVal) {
             curVal = neighVal;
             cur = neigh;
+            sinceNewCount = 0;
+        } else {
+            sinceNewCount += 1;
         }
-        return curVal;
+
+        if (curVal > bestVal) {
+            bestVal = curVal;
+            best = cur;
+        }
+
+        return bestVal;
     }
 
     /**
--- src/opt/SimulatedAnnealing.java
+++ src/opt/SimulatedAnnealing.java
@@ -10,27 +10,37 @@ import shared.Instance;
  * @version 1.0
  */
 public class SimulatedAnnealing extends OptimizationAlgorithm {
-    
+
     /**
      * The current optimiation data
      */
     private Instance cur;
-    
+
     /**
      * The current optimization value
      */
     private double curVal;
-    
+
+    // Track the best instance / value seen
+    private Instance best;
+    private double bestVal;
+
+    // Max number of iterations to go without finding a better max
+    private int SINCE_NEW_MAX = 10;
+
+    // Keep track of iterations since we found a new max
+    private int sinceNewCount = 0;
+
     /**
      * The current temperature
      */
     private double t;
-    
+
     /**
      * The cooling parameter
      */
     private double cooling;
-    
+
     /**
      * Make a new simulated annealing hill climbing
      * @param t the starting temperature
@@ -43,6 +53,8 @@ public class SimulatedAnnealing extends OptimizationAlgorithm {
         this.cooling = cooling;
         this.cur = hcp.random();
         this.curVal = hcp.value(cur);
+        best = cur;
+        bestVal = curVal;
     }
 
     /**
@@ -50,15 +62,31 @@ public class SimulatedAnnealing extends OptimizationAlgorithm {
      */
     public double train() {
         HillClimbingProblem p = (HillClimbingProblem) getOptimizationProblem();
+
+        if (sinceNewCount > SINCE_NEW_MAX) {
+            cur = p.random();
+            curVal = p.value(cur);
+            sinceNewCount = 0;
+        }
+
         Instance neigh = p.neighbor(cur);
         double neighVal = p.value(neigh);
-        if (neighVal > curVal || Distribution.random.nextDouble() < 
+        if (neighVal >= curVal || Distribution.random.nextDouble() <
                 Math.exp((neighVal - curVal) / t)) {
             curVal = neighVal;
             cur = neigh;
+            sinceNewCount = 0;
+        } else {
+            sinceNewCount += 1;
         }
         t *= cooling;
-        return curVal;
+
+        if (curVal > bestVal) {
+            bestVal = curVal;
+            best = cur;
+        }
+
+        return bestVal;
     }
 
     /**
