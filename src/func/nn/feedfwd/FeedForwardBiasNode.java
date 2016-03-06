4b6b06cec7b4b32eb1feeb63b7c49f01cbe22f6a
Bogdan Vatulya <bogdan.vatulya@organicresponse.com.au>
2016-03-06 16:02:45 +1100

    [fix] Do not connect bias node to other bias node. Subject to an assumption that bias backpropagation nodes can only be connected to backpropagation nodes and bias feedforward nodes can only be connected to feedforward nodes

--- src/func/nn/feedfwd/FeedForwardBiasNode.java
+++ src/func/nn/feedfwd/FeedForwardBiasNode.java
@@ -1,8 +1,9 @@
 package func.nn.feedfwd;
-import func.nn.feedfwd.FeedForwardNode;
+
+import func.nn.Neuron;
 
 /**
- * 
+ *
  * @author Jesse Rosalia <https://github.com/theJenix>
  * @date 2013-03-05
  */
@@ -14,7 +15,19 @@ public class FeedForwardBiasNode extends FeedForwardNode {
     }
 
     /**
-* @see nn.FeedForwardNode#feedforward()
-*/
+     * @see func.nn.feedfwd.FeedForwardNode#feedforward()
+     */
     public void feedforward() { }
+
+    /**
+     * Bias node should not be connected to other bias nodes
+     * @param neuron other neuron to connect to
+     */
+    @Override
+    public void connect(Neuron neuron) {
+        if (!neuron.getClass().equals(FeedForwardBiasNode.class)) {
+            super.connect(neuron);
+        }
+    }
+
 }
