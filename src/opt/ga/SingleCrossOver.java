c3e8f04649788cf586bc7e947bb21305d01256f2
Avriel Harvey <psu.ftbl@gmail.com>
2016-03-12 09:59:04 -0500

    Added JavaDoc to existing crossover implementations and added the two-point crossover implementation

--- src/opt/ga/SingleCrossOver.java
+++ src/opt/ga/SingleCrossOver.java
@@ -5,18 +5,30 @@ import dist.Distribution;
 import shared.Instance;
 
 /**
- * A single point cross over function
+ * Implementation of the single point crossover function for genetic algorithms.
+ *
  * @author Andrew Guillory gtg008g@mail.gatech.edu
  * @version 1.0
  */
 public class SingleCrossOver implements CrossoverFunction {
 
     /**
-     * @see opt.CrossOverFunction#mate(opt.OptimizationData, opt.OptimizationData)
+     * Mates two candidate solutions using single point crossover by choosing a point in the bit string, and creating
+     * a crossover mask of 0s up to that point, then 1s after. The mated solution takes the first bits from the second
+     * solution, and the remaining bits from the first.
+     *
+     * @param a the first solution
+     * @param b the second solution
+     * @return the mated solution
      */
     public Instance mate(Instance a, Instance b) {
+        // Create space for the mated solution
         double[] newData = new double[a.size()];
+
+        // Randomly assign the dividing point
         int point = Distribution.random.nextInt(newData.length + 1);
+
+        // Assign the bits for the mated solution
         for (int i = 0; i < newData.length; i++) {
             if (i >= point) {
                 newData[i] = a.getContinuous(i);
@@ -24,6 +36,8 @@ public class SingleCrossOver implements CrossoverFunction {
                 newData[i] = b.getContinuous(i);
             }
         }
+
+        // Return the mated solution
         return new Instance(newData);
     }
 
