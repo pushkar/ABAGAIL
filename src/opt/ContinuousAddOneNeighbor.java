6b24f83dd75a4b9e696509e9c5dac86505e9a54f
Chris Hodapp <Hodapp87@gmail.com>
2016-03-16 19:18:56 -0400

    Docs for ContinuousAddOneNeighbor and ContinuousAddOneMutation

--- src/opt/ContinuousAddOneNeighbor.java
+++ src/opt/ContinuousAddOneNeighbor.java
@@ -5,33 +5,40 @@ import dist.Distribution;
 import shared.Instance;
 
 /**
- * A continuous add one neighbor function
+ * A continuous add-one neighbor function which perturbs a randomly-chosen
+ * dimension of the input instance by adding to it some uniform random value
+ * in the range <code>[-amount/2,amount/2]</code>.
  * @author Andrew Guillory gtg008g@mail.gatech.edu
  * @version 1.0
  */
 public class ContinuousAddOneNeighbor implements NeighborFunction {
     /**
-     * The amount to add to the value
+     * The range by which to modify the value
      */
     private double amount;
     
     /**
-     * Continuous add one neighbor
-     * @param amount the amount to add
+     * Continuous add-one neighbor
+     * @param amount The range in which to perturb the instance
      */
     public ContinuousAddOneNeighbor(double amount) {
         this.amount = amount;
     }
     
     /**
-     * Continuous add one neighbor
+     * Continuous add-one neighbor with <code>amount=1</code>, that is,
+     * {@link #neighbor} will add a uniform random value in [-0.5, 0.5] 
+     * to some dimension.
      */
     public ContinuousAddOneNeighbor() {
         this(1);
     }
 
     /**
-     * @see opt.NeighborFunction#neighbor(opt.OptimizationData)
+     * Return a new {@link Instance} produced by perturbing one dimension of
+     * input <code>d</code> as described.
+     * @see opt.NeighborFunction#neighbor
+     * @param d Input value
      */
     public Instance neighbor(Instance d) {
         int i = Distribution.random.nextInt(d.size());
--- src/opt/ga/ContinuousAddOneMutation.java
+++ src/opt/ga/ContinuousAddOneMutation.java
@@ -5,32 +5,43 @@ import dist.Distribution;
 import shared.Instance;
 
 /**
- * A continuous add one neighbor function
+ * A continuous add-one mutation function which perturbs a randomly-chosen
+ * dimension of the input instance by adding to it some uniform random value
+ * in the range <code>[-amount/2,amount/2]</code>.
+ * 
+ * <p>
+ * Note that this is basically an in-place version of
+ * {@link opt.ContinuousAddOneNeighbor}.
  * @author Andrew Guillory gtg008g@mail.gatech.edu
  * @version 1.0
  */
 public class ContinuousAddOneMutation implements MutationFunction {
     /**
-     * The amount to add to the value
+     * The range by which to modify the value
      */
     private double amount;
     
     /**
-     * Continuous add one neighbor
-     * @param amount the amount to add
+     * Continuous add-one mutation
+     * @param amount The range in which to perturb the instance
      */
     public ContinuousAddOneMutation(double amount) {
         this.amount = amount;
     }
     
     /**
-     * Continuous add one neighbor
+     * Continuous add-one mutation with <code>amount=1</code>, that is,
+     * {@link #mutate} will add a uniform random value in [-0.5, 0.5] 
+     * to some dimension.
      */
     public ContinuousAddOneMutation() {
         this(1);
     }
 
     /**
+     * Modify input <code>d</code> by perturbing one of its dimensions as
+     * described.
+     * @param cod Input value
      * @see opt.ga.MutationFunction
      */
     public void mutate(Instance cod) {
