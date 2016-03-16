535d4a3abba32cdaa0e2e8874f177733f3d7f33f
Chris Hodapp <Hodapp87@gmail.com>
2016-03-16 14:36:17 -0400

    Added DiscreteChangeOneNeighbor docs.

--- src/opt/DiscreteChangeOneNeighbor.java
+++ src/opt/DiscreteChangeOneNeighbor.java
@@ -5,7 +5,9 @@ import dist.Distribution;
 import shared.Instance;
 
 /**
- * A neighbor function for changing a single value
+ * A neighbor function which changes a single value in an {@link Instance} -
+ * specifically, it changes a randomly-chosen dimension to a random integer in
+ * a given range.  Ranges are set per-dimension.
  * @author Andrew Guillory gtg008g@mail.gatech.edu
  * @version 1.0
  */
@@ -17,15 +19,24 @@ public class DiscreteChangeOneNeighbor implements NeighborFunction {
     private int[] ranges;
     
     /**
-     * Make a new change one neighbor function
-     * @param ranges the ranges of the data
+     * Make a new change-one neighbor function.  In the given ranges, element
+     * <code>i</code> of the array gives the range of dimension <code>i</code>
+     * of the input {@link Instance}.
+     * <p>
+     * A value of <code>N</code> in the range corresponds to integers from 0 to
+     * <code>N-1</code>, inclusive.
+     * @param ranges the ranges of the data; should have as many elements as
+     * dimensions of input {@link Instance} to {@link #neighbor}.
      */
     public DiscreteChangeOneNeighbor(int[] ranges) {
         this.ranges = ranges;
     }
     
     /**
-     * @see opt.NeighborFunction#neighbor(opt.OptimizationData)
+     * @return A neighboring {@link Instance} in which one randomly chosen
+     * dimension of <code>d</code> is set to a random value inside of the
+     * corresponding range.
+     * @see opt.NeighborFunction#neighbor
      */
     public Instance neighbor(Instance d) {
         Instance cod = (Instance) d.copy();
