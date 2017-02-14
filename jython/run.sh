05d97092cda01a9e06a0698852ab0bd670808046
Ryan Peach <rgpeach10@gmail.com>
2017-02-14 14:23:10 +0000

    Added Abagail

--- jython/run.sh
+++ jython/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # edit the classpath to to the location of your ABAGAIL jar file
 #
-export CLASSPATH=./lib/ABAGAIL.jar:$CLASSPATH
+export CLASSPATH=../ABAGAIL.jar:$CLASSPATH
 mkdir -p data/plot logs image
 
 # four peaks
@@ -23,3 +23,7 @@ jython knapsack.py
 # abalone test
 echo "Running abalone test"
 jython abalone_test.py
+
+# traveling salesman
+echo "Running traveling salesman test"
+jython travelingsalesman.py
