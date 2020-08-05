1298aaf0a91484ba63628b03eb86ba7ec775566c
John Mansfield <jmansfield6@gatech.edu>
2020-08-05 06:43:03 -0400

    adding .bat

---
+++ jython/run.bat
@@ -0,0 +1,27 @@
+:: edit the classpath to the location of your ABAGAIL jar file
+set CLASSPATH=ABAGAIL.jar
+mkdir -p data\plot logs image
+
+:: four peaks
+echo "four peaks"
+jython fourpeaks.py
+
+:: count ones
+echo "count ones"
+jython countones.py
+
+:: continuous peaks
+echo "continuous peaks"
+jython continuouspeaks.py
+
+:: knapsack
+echo "Running knapsack"
+jython knapsack.py
+
+:: abalone test
+echo "Running abalone test"
+jython abalone_test.py
+
+:: TSP
+echo "Running TSP test"
+jython travelingsalesman.py
